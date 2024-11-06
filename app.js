24a47707e5b8840dcd6c9f03a312abc376950f05
Johnson Chong <23009886@myrp.edu.sg>
2024-11-07 05:40:51 +0800

    Update app.js
    public pt2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const data = require('./data');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
